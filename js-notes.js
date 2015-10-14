5f4b700e37237aba2c85f955a0de76cb7f982107
illallstar <johnobagge@gmail.com>
2015-10-13 20:25:09 -0400

    Studying, making notes, initial commit

---
+++ js-notes.js
@@ -0,0 +1,19 @@
+// examples of pop up boxes:
+// confirm("I am ok");
+// prompt(Are you ok?");
+
+// date types:
+// numbers... 1,2,etc
+// strings... "Red dogs are cool!" 
+// booleans... false, 2 >= 3
+
+// conditionals
+// if the frst condition is met, execute first code block, if it is false, execute the code in the else block, example:
+if (12/4 == "Hey".length) 
+{
+confirm("Will this run the first block?");
+}
+else
+{
+confirm(Or the second block?");
+}
