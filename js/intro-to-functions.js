526a382f4a8423be99f4b946641a8beab1f83a3b
illallstar <johnobagge@gmail.com>
2015-10-21 19:32:41 -0400

    made folders

---
+++ js/intro-to-functions.js
@@ -0,0 +1,114 @@
+/* 
+var speed = 65;
+
+if (speed > 80) {
+	// Use console.log() to print "Slow down"
+	console.log("Slow down");
+} 
+else {
+	// Use console.log() to print "Drive safe"
+console.log("Drive safe");
+
+}
+*/
+
+/*
+var divideByThree = function (number) {
+    var val = number / 3;
+    console.log(val);
+};
+
+divideByThree(9);
+*/
+
+/*
+var greeting = function (name) {
+    console.log("Great to see you," + " " + name);
+};
+
+greeting("John");
+*/
+
+/*
+var foodDemand = function(food) {
+    console.log("I want to eat" + " " + food);
+    
+};
+
+foodDemand("cookies");
+*/
+
+/*
+var calculate = function (number) {
+    var val = number * 10;
+    console.log(val);
+};
+
+var greeting = function (name) {
+    var val = ("Hello" + " ") + name ;
+    console.log(val);
+};
+
+greeting("John")
+*/
+
+/*
+var orangeCost = function (price) {
+    var val = price*5 ;
+    console.log(val);
+};
+    
+orangeCost("5");    
+*/
+
+/*
+var timesTwo = function(number) {
+    return number * 2;
+};
+
+var newNumber = timesTwo(4);
+
+console.log(newNumber);
+*/
+
+/*
+var quarter = function (number) {
+    var val = number / 4 ;
+    return(val);
+}
+
+if (quarter(12) % 3 === 0 ) {
+  console.log("The statement is true");
+} else {
+  console.log("The statement is false");
+}
+
+quarter(12)
+*/
+
+/*
+var perimeterBox = function(length, width) {
+    return length + length + width + width;
+}
+
+perimeterBox(12,12,6,6);
+*/
+    
+/*
+var my_number = 7; //this has global scope
+
+var timesTwo = function(number) {
+    var my_number = number * 2;
+    console.log("Inside the function my_number is: ");
+    console.log(my_number);
+}; 
+
+timesTwo(7);
+
+console.log("Outside the function my_number is: ")
+console.log(my_number);
+*/
+
+/*
+
+
---
+++ js/js-notes.js
@@ -0,0 +1,31 @@
+// examples of pop up boxes:
+// confirm("I am ok");
+// prompt(Are you ok?");
+
+// date types:
+// numbers... 1,2,etc
+// strings... "Red dogs are cool!" 
+// booleans... false, 2 >= 3
+
+// conditionals
+// if the frst condition is met, execute first code block, 
+// if it is false, execute the code in the else block, example:
+
+if (12/4 == "Hey".length) 
+{
+confirm("Will this run the first block?");
+}
+else
+{
+confirm("Or the second block?");
+}
+
+//test notes for git
+
+//another git test from command line and nano
+
+//more notes...
+//variables... store data value, we can show the data by typing the variable name
+//manipulating numbers and strings... comparisons, >, == etc., modulo %, string length, sub string
+
+//console.log()... prints into the console what's up in those ()! 
